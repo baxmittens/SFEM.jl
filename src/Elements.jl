ab7938c367f9b6a954e5136d7f8496349d0523ca
Max Bittens <12096700+StreetLevel@users.noreply.github.com>
2025-10-01 10:49:24 +0200

    add src folder

---
+++ src/Elements.jl
@@ -0,0 +1,10 @@
+module Elements
+
+abstract type GenericElement
+end
+abstract type ContinuumElement <: GenericElement
+end
+abstract type ContactElement <: GenericElement
+end
+
+end #module Elements
